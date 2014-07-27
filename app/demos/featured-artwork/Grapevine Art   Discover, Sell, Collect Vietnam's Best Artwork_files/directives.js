5f52f7e89fa8ff471418521947042edb2b8e3566
Son Tran Lam <sontl@mac.com>
2014-07-27 18:06:57 +0800

    Add resources files for mockup description and Artwork from Saatchi

---
+++ app/demos/featured-artwork/Grapevine Art   Discover, Sell, Collect Vietnam's Best Artwork_files/directives.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/* Directives */
+
+
+angular.module('myApp.directives', []).
+    directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+    }]).
+    directive('artSwitcher', [ function () {
+        return {
+            restrict : 'AEC',
+            link: function (scope, element, attrs) { // function content is optional
+                console.log("template switcher generated");
+            },
+            templateUrl : 'partials/switcher.html'
+        };
+    }]).
+    directive('artWrapper', [ function () {
+        return {
+            restrict : 'ACE',
+            link: function (scope, element, attrs) { // function content is optional
+                console.log("template wrapper generated");
+            },
+            templateUrl : 'partials/wrapper.html'
+        };
+    }]).
+    directive('artFeaturedArtworks', [ function () {
+        return {
+            restrict : 'ACE',
+            link: function (scope, element, attrs) { // function content is optional
+                console.log("featured artwork generated");
+            },
+            templateUrl : 'partials/featured-artwork-1.html'
+        };
+    }]).
+    directive('artWrapperSlider', [ function () {
+        return {
+            restrict : 'ACE',
+            link: function (scope, element, attrs) { // function content is optional
+                console.log("wrapper slider generated");
+            },
+            templateUrl : 'partials/wrapper-slider-1.html'
+        };
+    }]).
+    directive('artWrapperGallery4Columns', [ function () {
+        return {
+            restrict : 'ACE',
+            link: function (scope, element, attrs) { // function content is optional
+                console.log("artWrapperGallery4Columns generated");
+            },
+            templateUrl : 'partials/wrapper-gallery-4-columns.html'
+        };
+    }]);
