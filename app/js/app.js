365ec1d81ca98a8c6d99f18d4aec63578a945201
Son Tran Lam <sontl@mac.com>
2014-08-20 01:01:25 +0800

    add fileupload and some more libs

--- app/js/app.js
+++ app/js/app.js
@@ -14,6 +14,7 @@ angular.module('myApp', [
         $routeProvider.when('/browse', {templateUrl: 'partials/partial1.html', controller: 'BrowseCtrl'});
         $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
         $routeProvider.when('/upload', {templateUrl: 'partials/upload-images.html', controller: 'UploadCtrl'});
+        $routeProvider.when('/simpleupload', {templateUrl: 'partials/simple-upload.html', controller: 'UploadCtrl'});
         $routeProvider.otherwise({redirectTo: '/home'});
         // remove hash-tag # in URL
 //        if(window.history && window.history.pushState){
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', ['angularFileUpload'])
+angular.module('myApp.controllers', ['angularFileUpload', 'ui.bootstrap', 'ngTagsInput'])
     .controller('BrowseCtrl', ['$scope', function ($scope) {
 
     }])
@@ -20,7 +20,7 @@ angular.module('myApp.controllers', ['angularFileUpload'])
         $scope.footer = $scope.templates[1];
         $scope.topbar = $scope.templates[2];
     }])
-    .controller('UploadCtrl', ['$scope', 'FileUploader', function($scope, FileUploader) {
+    .controller('UploadCtrl', ['$scope', 'FileUploader',  function($scope, FileUploader, $http) {
         var uploader = $scope.uploader = new FileUploader({
             url: 'http://localhost:8080/v1/object'
         });
@@ -71,6 +71,104 @@ angular.module('myApp.controllers', ['angularFileUpload'])
         };
 
         console.info('uploader', uploader);
+
+        $scope.oneAtATime = true;
+
+        $scope.groups = [
+            {
+                title: 'Dynamic Group Header - 1',
+                content: 'Dynamic Group Body - 1'
+            },
+            {
+                title: 'Dynamic Group Header - 2',
+                content: 'Dynamic Group Body - 2'
+            }
+        ];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function() {
+            var newItemNo = $scope.items.length + 1;
+            $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.status = {
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+
+        $scope.isHideUploadStandards = false;
+
+        $scope.hideUploadStandards = function() {
+            $scope.isHideUploadStandards = !$scope.isHideUploadStandards;
+            $scope.currentTpl='artwork-details.html';
+        }
+
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.initDate = new Date('2016-15-20');
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+        $scope.items = [
+            'The first choice!',
+            'And another choice for you.',
+            'but wait! A third!'
+        ];
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function(open) {
+            console.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+
+        $scope.tags = [
+            { text: 'just' },
+            { text: 'some' },
+            { text: 'cool' },
+            { text: 'tags' }
+        ];
+        $scope.loadTags = function(query) {
+            return $http.get('/tags?query=' + query);
+        };
+
     }]);
 
 
